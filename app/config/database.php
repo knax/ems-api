4a92f6aa53c3bcc32ddccb4a4028bf2088c08c23
Hasta Ragil <npcmamaz@gmail.com>
2014-03-02 00:25:12 +0700

    Changed database file to match travis

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'testing',
-			'username'  => 'testing',
-			'password'  => 'testing',
+			'database'  => 'travis',
+			'username'  => 'travis',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
