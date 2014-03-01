f65590d7324297374b79156785a0b0a7919faefb
Hasta Ragil <npcmamaz@gmail.com>
2014-03-02 05:06:59 +0700

    Removed timestamps for users table

--- app/tests/_data/dump.sql
+++ app/tests/_data/dump.sql
@@ -1,8 +1,7 @@
-/* Replace this file with actual dump of your database */
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_username_unique` (`username`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
