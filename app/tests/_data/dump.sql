1c964b7deb3c52b1baabed2f111d21a49c52766c
Hasta Ragil <npcmamaz@gmail.com>
2014-03-02 00:25:42 +0700

    Added dumped data to codeception

--- app/tests/_data/dump.sql
+++ app/tests/_data/dump.sql
@@ -1,1 +1,8 @@
 /* Replace this file with actual dump of your database */
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_username_unique` (`username`)
+);
