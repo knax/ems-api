eb00e774021509adbc47ec0f4a7ea97e9785862d
Hasta Ragil <npcmamaz@gmail.com>
2014-03-02 05:07:20 +0700

    Fixed homepage test

--- app/tests/api/HomepageCest.php
+++ app/tests/api/HomepageCest.php
@@ -4,8 +4,15 @@ use \ApiGuy;
 class HomepageCest
 {
 
+	private $username = 'someuser';
+
+	private $password = 'somepass';
+
+	private $hashedPassword = '';
+
 	public function _before()
 	{
+		$this->hashedPassword = password_hash($this->password, PASSWORD_BCRYPT, array('cost' => 10));
 	}
 
 	public function _after()
@@ -13,11 +20,12 @@ class HomepageCest
 	}
 
 	public function homepageWithAuthencation(ApiGuy $I) {
+		$I->haveInDatabase('users', array('username' => $this->username, 'password' => $this->hashedPassword));
 		$I->wantTo('see the homepage of the api with authencation');
-		$I->amHttpAuthenticated('firstuser','first_password');
+		$I->amHttpAuthenticated($this->username, $this->password);
 		$I->sendGET('');
 		$I->seeResponseCodeIs(200);
-		$I->seeResponseContainsJson(['error' => false, 'message' => 'welcome firstuser']);
+		$I->seeResponseContainsJson(['error' => false, 'message' => 'welcome '.$this->username]);
 	}
 
 	public function homepageWithoutAuthencation(ApiGuy $I) {
