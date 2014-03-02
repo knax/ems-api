452ee029f23a0bc3c1f846d74bb67204afb7f5a3
Hasta Ragil <npcmamaz@gmail.com>
2014-03-02 23:38:53 +0700

    Used migrations instead dump.sql on codeception

--- app/tests/_data/dump.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_username_unique` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
