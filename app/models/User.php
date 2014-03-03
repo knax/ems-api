023f2e5b1b64dc6ba96848bc7650bb11ac7b64ba
Hasta Ragil <npcmamaz@gmail.com>
2014-03-03 22:26:21 +0700

    Use namespace on model

--- app/models/User.php
+++ app/models/User.php
@@ -1,9 +1,11 @@
 <?php
 
+namespace Model;
+
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends \Eloquent implements UserInterface, RemindableInterface {
 
 	public $timestamps = false;
 
